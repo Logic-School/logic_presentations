997a36c230538d6ceb3b7db3b5d7564aa92f0162
argenspin <argenspin@gmail.com>
2023-09-29 14:25:39 +0530

    added total and presented students count

--- models/presentations.py
+++ models/presentations.py
@@ -2,6 +2,7 @@ from odoo import fields,models,api
 
 class LogicPresentation(models.Model):
     _name="logic.presentations"
+    _description="Presentation"
     name = fields.Char(string="Name",compute="_compute_name")
     def _compute_name(self):
         for record in self:
@@ -13,6 +14,20 @@ class LogicPresentation(models.Model):
     batch_id = fields.Many2one('logic.base.batch',string="Batch",required=True)
     date = fields.Date(string="Date",required=True)
     student_presentations = fields.One2many('logic.student.presentation','presentation_id',string='Presentations')
+    total_students = fields.Integer(string="Total Strength",compute="get_total_students" ,readonly=True)
+
+    @api.depends('batch_id')
+    def get_total_students(self):
+        # for record in self:
+        if self.batch_id:
+            self.total_students = self.env['logic.students'].search_count([('batch_id','=',self.batch_id.id)])
+        else:
+            self.total_students = 0
+    presentation_count = fields.Integer(string="Students Presented",compute="_compute_presentation_count")
+    def _compute_presentation_count(self):
+        for record in self:
+            record.presentation_count = len(record.student_presentations)
+
 class StudentPresentation(models.Model):
     _name="logic.student.presentation"
     student_id = fields.Many2one('logic.students',string="Student",required=True)
